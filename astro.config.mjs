01dd5e718b7bdeac431e22e357380b27888388cd
Dawid Drozdz <dawid.drozdz@example.com>
2026-07-30 12:43:38 +0200

    Initial commit: Il Momento ristorante site

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://dawiddroz.github.io',
+  base: '/il-momento-velletri',
+  devOptions: {
+    host: true
+  }
+});
